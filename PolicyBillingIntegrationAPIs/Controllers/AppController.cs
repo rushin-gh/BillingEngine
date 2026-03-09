2a13b8a5e872543da1ebd19709d55b05d6185cd7
rushin-gh <rushin.mailme@gmail.com>
2026-03-09 23:43:07 +0530

    Add 2 global controllers

---
+++ PolicyBillingIntegrationAPIs/Controllers/AppController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PolicyBillingIntegrationAPIs.Controllers
+{
+    [ApiController]
+    public class AppController : ControllerBase
+    {
+        [HttpGet("health")]
+        public string ping() =>  "App is running successfully";
+    }
+}
---
+++ PolicyBillingIntegrationAPIs/Controllers/PolicyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PolicyBillingIntegrationAPIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PolicyController : ControllerBase
+    {
+
+    }
+}
